c03fc558fc7539398c6c86fda00116be2b0cd928
lesterli <fulfilself@gmail.com>
2023-12-14 10:37:36 +0000

    fix bug: jq

--- scripts/init_and_start.sh
+++ scripts/init_and_start.sh
@@ -8,7 +8,6 @@ CHAINID=$3
 PEERS=$4
 GENESIS_FILE=$5
 DATA_DIR=/data
-IBC_TOKEN_DENOM=`jq '.app_state.crisis.constant_fee.denom' $GENESIS_FILE`
 
 if [ ! -f "$DATA_DIR/config/config.toml" ]; then
 	# Initialize node's configuration files.
@@ -17,11 +16,14 @@ if [ ! -f "$DATA_DIR/config/config.toml" ]; then
 	# Modify the persistent_peers field of config.toml
 	sed -i.bak "s/persistent_peers = \"\"/persistent_peers = \"${PEERS}\"/" $DATA_DIR/config/config.toml
 
-    # Modify the minimum-gas-price field of app.toml
-    sed -i.bak "s#minimum-gas-prices = \"0aotto\"#minimum-gas-prices = \"20000000000${IBC_TOKEN_DENOM}\"#" $DATA_DIR/config/app.toml
-
 	# Download genesis file
 	curl -L -o $DATA_DIR/config/genesis.json $GENESIS_FILE
+
+	# Get the IBC_TOKEN_DENOM from genesis.json
+	IBC_TOKEN_DENOM=`jq '.app_state.crisis.constant_fee.denom' $DATA_DIR/config/genesis.json`
+
+	# Modify the minimum-gas-price field of app.toml
+	sed -i.bak "s#minimum-gas-prices = \"0aotto\"#minimum-gas-prices = \"20000000000${IBC_TOKEN_DENOM}\"#" $DATA_DIR/config/app.toml
 
 	# Copy cosmovisor folder to data directory
 	cp -R /root/cosmovisor $DATA_DIR/
